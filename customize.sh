804971038ec42a7943a3c3bf4210c32ed04f3686
cced1900 <cced4400@gmail.com>
2021-09-09 10:59:28 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,7 +2,7 @@
 #=================================================
 # Description: Build OpenWrt using GitHub Actions
 # Lisence: MIT
-# Author: LinSir
+# Author: LinSir 
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.1.252/g' openwrt/package/base-files/files/bin/config_generate
